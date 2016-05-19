558be5b5c43d20baf2de2d6036bce806e4887bce
Faiz Siddiqui <faize.sid@gmail.com>
2016-05-19 21:31:24 +0530

    Socket functionality added.

--- app/src/main/java/com/major/dustinclient/MainActivity.java
+++ app/src/main/java/com/major/dustinclient/MainActivity.java
@@ -4,15 +4,22 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -26,8 +33,21 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStates;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Locale;
 
 /**
  * Created by Faiz on 05-05-2016.
@@ -36,36 +56,41 @@ public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        OnMapReadyCallback {
 
     private final static String TAG = "MainActivity";
-    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
-    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
-    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
-    protected final static String LOCATION_KEY = "location-key";
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    private final static String LOCATION_KEY = "location-key";
     private static final int REQUEST_LOCATION = 0;
     private static String[] PERMISSION_LOCATION = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     private GoogleApiClient mGoogleApiClient;
-    protected LocationRequest mLocationRequest;
-    protected Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private GoogleMap mMap;
 
-    protected View parentLayout;
-    protected TextView mLatitude, mLongitude;
+    private String HOST;
+    private Integer PORT;
+    private Socket socket;
+    private DataOutputStream dataOutputStream;
 
-    protected String mLatitudeLabel;
-    protected String mLongitudeLabel;
+    private View parentLayout;
+    private TextView mLatitude, mLongitude, mIP;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         parentLayout = findViewById(R.id.parent_layout);
+        mIP = (TextView) findViewById(R.id.IP);
         mLatitude = (TextView) findViewById(R.id.latitude);
         mLongitude = (TextView) findViewById(R.id.longitude);
-
-        mLatitudeLabel = getResources().getString(R.string.latitude_label);
-        mLongitudeLabel = getResources().getString(R.string.longitude_label);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -81,15 +106,14 @@ public class MainActivity extends AppCompatActivity implements
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(mLocationRequest);
 
-        final PendingResult<LocationSettingsResult> result =
+        final PendingResult<LocationSettingsResult> locationResult =
                 LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
                         builder.build());
 
-        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+        locationResult.setResultCallback(new ResultCallback<LocationSettingsResult>() {
             @Override
             public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
                 final Status status = locationSettingsResult.getStatus();
-                final LocationSettingsStates locationSettingsStates = locationSettingsResult.getLocationSettingsStates();
                 switch (status.getStatusCode()) {
                     case LocationSettingsStatusCodes.SUCCESS:
                         if (savedInstanceState != null) {
@@ -102,9 +126,7 @@ public class MainActivity extends AppCompatActivity implements
                     case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
                         try {
                             status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
-
-                        } catch (IntentSender.SendIntentException e) {
-
+                        } catch (IntentSender.SendIntentException ignored) {
                         }
                         break;
                     case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
@@ -112,12 +134,25 @@ public class MainActivity extends AppCompatActivity implements
                 }
             }
         });
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        HOST = prefs.getString(getResources().getString(R.string.pref_key_ip_address), "");
+        PORT = Integer.parseInt(prefs.getString(getResources().getString(R.string.pref_key_port_address), "0"));
+
     }
 
     private void updateUI() {
         if (mCurrentLocation != null) {
-            mLatitude.setText(String.format("%s: %f", mLatitudeLabel, mCurrentLocation.getLatitude()));
-            mLongitude.setText(String.format("%s: %f", mLongitudeLabel, mCurrentLocation.getLongitude()));
+            mLatitude.setText(String.format(Locale.ENGLISH, getResources().getString(R.string.latitude), mCurrentLocation.getLatitude()));
+            mLongitude.setText(String.format(Locale.ENGLISH, getResources().getString(R.string.longitude), mCurrentLocation.getLongitude()));
+            JSONObject jsonData = new JSONObject();
+            try {
+                jsonData.put(getResources().getString(R.string.LATITUDE), mCurrentLocation.getLatitude());
+                jsonData.put(getResources().getString(R.string.LONGITUDE), mCurrentLocation.getLongitude());
+                new SocketClient().execute(jsonData);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -129,7 +164,6 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
         switch (requestCode) {
             case REQUEST_CHECK_SETTINGS:
                 switch (resultCode) {
@@ -185,6 +219,47 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (dataOutputStream != null) {
+            try {
+                dataOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, SettingsActivity.GeneralPreferenceFragment.class.getName());
+                intent.putExtra(PreferenceActivity.EXTRA_NO_HEADERS, true);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.i(TAG, "Connected to GoogleApiClient");
         if (mCurrentLocation == null) {
@@ -211,6 +286,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
+        setLocation(mCurrentLocation);
         updateUI();
     }
 
@@ -248,4 +324,57 @@ public class MainActivity extends AppCompatActivity implements
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (mCurrentLocation != null) {
+            setLocation(mCurrentLocation);
+        }
+    }
+
+    public void setLocation(Location location) {
+        LatLng dustInLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(dustInLocation).title("DustIn Location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(dustInLocation, 17.0f));
+    }
+
+    public class SocketClient extends AsyncTask<JSONObject, Void, Void> {
+
+        private JSONObject data;
+
+        @Override
+        protected Void doInBackground(JSONObject... params) {
+            data = params[0];
+            if (socket == null) {
+                if (!HOST.isEmpty() && !PORT.equals(0)) {
+                    try {
+                        socket = new Socket(HOST, PORT);
+                        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                if (socket.isConnected()) {
+                    try {
+                        dataOutputStream.writeUTF(data.toString());
+                        Log.d("Socket Data", data.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.d("Socket Status", "Failed");
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mIP.setText(String.format(getResources().getString(R.string.socket_address), HOST, PORT));
+        }
+    }
+
 }
